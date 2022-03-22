7d8630f632971f8f6cdbaed7e6aebceb857c07c0
modridi <mohamed-amine.dridi@camptocamp.com>
2022-03-22 11:46:04 +0100

    fix: json_pointers parameter name

--- main.tf
+++ main.tf
@@ -63,7 +63,7 @@ resource "argocd_application" "this" {
       group = "admissionregistration.k8s.io"
       kind = "ValidatingWebhookConfiguration"
       name = "admissionregistration.k8s.io"
-      jsonPointers = ["/webhooks/0/namespaceSelector/matchExpressions/2"]
+      json_pointers = ["/webhooks/0/namespaceSelector/matchExpressions/2"]
     }
 
     sync_policy {
