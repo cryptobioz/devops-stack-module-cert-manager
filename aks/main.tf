d9fa4ce77fbe29070490180556fde6bb1f6d2dd1
modridi <mohamed-amine.dridi@camptocamp.com>
2022-03-23 17:40:26 +0100

    feat: add role assignment DNS zone contributor

--- aks/main.tf
+++ aks/main.tf
@@ -6,9 +6,20 @@ data "azurerm_subscription" "primary" {
 }
 
 resource "azurerm_user_assigned_identity" "cert_manager" {
-  resource_group_name = data.azurerm_resource_group.this.name
+  resource_group_name = var.node_resource_group_name
   location            = data.azurerm_resource_group.this.location
   name                = "cert-manager"
+}
+
+data "azurerm_dns_zone" "this" {
+  name                = var.base_domain
+  resource_group_name = var.resource_group_name
+}
+
+resource "azurerm_role_assignment" "dns_zone_contributor" {
+  scope                = data.azurerm_dns_zone.this.id
+  role_definition_name = "DNS Zone Contributor"
+  principal_id         = azurerm_user_assigned_identity.cert_manager.principal_id
 }
 
 module "cert-manager" {
