3bcdd8e1f871c5f1f8f65c8e2c06e907abd29433
Léo Depriester <leo.depriester@camptocamp.com>
2022-11-23 12:39:17 +0100

    feat: add module crds

---
+++ crds/main.tf
@@ -0,0 +1,31 @@
+data "helm_template" "this" {
+  name       = "cert-manager"
+  namespace  = "default"
+  repository = "https://charts.jetstack.io"
+
+  chart   = "cert-manager"
+  version = var.chart_version
+
+  include_crds = "true"
+
+  set {
+    name  = "installCRDs"
+    value = "true"
+  }
+}
+
+locals {
+  crds = merge([
+    for key, value in data.helm_template.this.manifests : merge([
+      for crd_value in split("\n---", value) : {
+        yamldecode(crd_value).metadata.name = yamldecode(crd_value)
+      }
+      if length(regexall("kind: CustomResourceDefinition", value)) > 0
+    ]...)
+  ]...)
+}
+
+resource "kubernetes_manifest" "crds" {
+  for_each = local.crds
+  manifest = merge({ for k, v in each.value : k => v if k != "status" }, { "metadata" = { for k, v in each.value.metadata : k => v if k != "creationTimestamp" } })
+}
